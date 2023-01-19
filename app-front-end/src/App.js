5f2ca3a4e04f2e4dcb56d9d18a2b6e6fd6b7518d
Abbas Dahodwala <abbas.dahodwala@mindstix.com>
2023-01-19 12:04:20 +0530

    Initial Project Setup

---
+++ app-front-end/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
+
+/* 
+Header
+<routers>
+- Home
+- Product Page
+- Reels
+
+</>
+ Footer
+ */
---
+++ app-front-end/src/components/Header.js
@@ -0,0 +1,4 @@
+import classes from "./Header.module.css";
+const Header = () => {};
+
+export default Header;
